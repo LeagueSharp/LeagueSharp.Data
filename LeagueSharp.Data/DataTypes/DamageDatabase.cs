d0c902b9253f90b3c0ce62a7d7439f04c5c06428
Exory <drago.enrico@hotmail.it>
2016-05-22 12:59:35 +0200

    Update DamageDatabase.cs

--- LeagueSharp.Data/DataTypes/DamageDatabase.cs
+++ LeagueSharp.Data/DataTypes/DamageDatabase.cs
@@ -25,7 +25,7 @@
         ///     The damage version files.
         /// </summary>
         internal static readonly IDictionary<string, byte[]> DamageFiles = new Dictionary<string, byte[]>
-                                                                               { { "6.9", Resources._6_9 } };
+                                                                               { { "6.10", Resources._6_10 } };
 
         #endregion
 
@@ -93,7 +93,7 @@
         /// </summary>
         void IDataType.Initialize()
         {
-            var version = new Version("6.9");
+            var version = new Version("6.10");
             var versionString = $"{version.Major}.{version.Minor}";
 
             var fileBytes = DamageFiles.ContainsKey(versionString)
